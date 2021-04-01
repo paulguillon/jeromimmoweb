7df47a43f01af6a5f0f1bcb9e11c4389cdba68fc
Breesho <felix.noel3@orange.fr>
2021-04-01 14:39:42 +0200

    maj css, logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/home';
 import PropertyList from './pages/property/property-list';
 import PropertyDetail from './pages/property/property-detail';
-
 import AgencyList from './pages/agency/agency-list';
 import AgencyDetail from './pages/agency/agency-detail';
-
 import Login from './components/auth/login';
 import HeaderNavigation from './components/template/navigation';
 import FooterNavigation from './components/template/footer';
@@ -22,7 +20,7 @@ const App: FunctionComponent = () => {
                 <Route path="/property/:idProperty" component={PropertyDetail} />
                 <Route exact path="/agencies" component={AgencyList} />
                 <Route path="/agency/:idAgency" component={AgencyDetail} />
-                <Route path="/" component={Home}/>
+                <Route path="/" component={Home} />
             </Switch>
             <FooterNavigation />
         </Router>
