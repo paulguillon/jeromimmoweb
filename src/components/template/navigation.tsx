8b9049ef118229a033e6c4b9e9272a22532df339
Breesho <felix.noel3@orange.fr>
2021-04-01 11:05:44 +0200

    maj dossier template nav + footer

---
+++ src/components/template/navigation.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+function HeaderNavigation() {
+    return (
+        <nav className="navbar navbar-expand-lg perso-black sticky-top">
+            <div className="container-fluid">
+                <img src="../../assets/img/logo.jpg" alt="logo" width="50px" height="50px" />
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <i className="fas fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto">
+                        <li><Link to="/">Accueil</Link></li>
+                        <li><Link to="/pages/property-list">Liste propriétés</Link></li>
+                        <li><Link to="/pages/agency-list">Liste des agences</Link></li>
+                    </ul>
+                    <ul className="navbar-nav d-flex justify-content-lg-end align-content-end">
+                        <li><Link to="/pages/login">Connexion</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default HeaderNavigation;
