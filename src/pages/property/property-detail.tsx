4d16b2aea0fad2fb86d3a4a335a9e3c64e57705b
Paul Guillon <alentak27@gmail.com>
2021-05-07 14:29:06 +0200

    back buttons

--- src/pages/property/property-detail.tsx
+++ src/pages/property/property-detail.tsx
@@ -1,29 +1,45 @@
-import React, { FunctionComponent, useState, useEffect } from 'react';
-import { RouteComponentProps, Link } from 'react-router-dom';
-import Property from '../../models/property/property';
-import PropertyCardDetail from '../../components/property/property-card-detail';
-import PropertyService from '../../services/property-service';
-import Loader from '../../components/loader';
- 
+import React, { FunctionComponent, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { RouteComponentProps, Link } from "react-router-dom";
+import Property from "../../models/property/property";
+import PropertyCardDetail from "../../components/property/property-card-detail";
+import PropertyService from "../../services/property-service";
+import Loader from "../../components/loader";
+
 type Params = { idProperty: string };
- 
-const PropertyDetail: FunctionComponent<RouteComponentProps<Params>> = ({ match }) => {
-   
-  const [property, setProperty] = useState<Property|null>(null);
- 
+
+const PropertyDetail: FunctionComponent<RouteComponentProps<Params>> = ({
+  match,
+}) => {
+  const [property, setProperty] = useState<Property | null>(null);
+  const history = useHistory();
+
   useEffect(() => {
-    PropertyService.getProperty(+match.params.idProperty).then(property => setProperty(property));
+    PropertyService.getProperty(+match.params.idProperty).then((property) =>
+      setProperty(property)
+    );
   }, [match.params.idProperty]);
-   
+
   return (
     <div>
-      { property ? (
-        <PropertyCardDetail key={property.idProperty} property={property} />
+      {property ? (
+        <div>
+          <PropertyCardDetail key={property.idProperty} property={property} />
+
+          <button
+            className="btn btn-secondary"
+            onClick={() => history.push(`/property`)}
+          >
+            Retour
+          </button>
+        </div>
       ) : (
-        <h4 className="center"><Loader /></h4>
+        <h4 className="center">
+          <Loader />
+        </h4>
       )}
     </div>
   );
-}
- 
+};
+
 export default PropertyDetail;
